30dbb643f595f8f549169c7a20ffa3549b3f58fe
Dictor1488 <168811367+Dictor1488@users.noreply.github.com>
2026-07-06 17:08:06 +0300

    Update entry log for Gameface renderer

--- python/gui/mods/mod_flyingdamage.py
+++ python/gui/mods/mod_flyingdamage.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
-# mod_flyingdamage.py  --  Python 2.7 entry point (native GUI renderer)
+# mod_flyingdamage.py  --  Python 2.7 entry point (Gameface renderer)
 
 import logging
 
 logger = logging.getLogger(__name__)
-logger.info('[FlyingDamage] === module imported (native GUI renderer) ===')
+logger.info('[FlyingDamageGF] === module imported (Gameface renderer) ===')
 
 
 def init():
-    logger.info('[FlyingDamage] init() called')
+    logger.info('[FlyingDamageGF] init() called')
     try:
         from flyingdamage import g_controller
         g_controller.init()
-        logger.info('[FlyingDamage] init() OK')
+        logger.info('[FlyingDamageGF] init() OK')
     except Exception:
-        logger.error('[FlyingDamage] init() CRASHED', exc_info=True)
+        logger.error('[FlyingDamageGF] init() CRASHED', exc_info=True)
 
 
 def fini():
-    logger.info('[FlyingDamage] fini() called')
+    logger.info('[FlyingDamageGF] fini() called')
     try:
         from flyingdamage import g_controller
         g_controller.fini()
     except Exception:
-        logger.error('[FlyingDamage] fini() CRASHED', exc_info=True)
+        logger.error('[FlyingDamageGF] fini() CRASHED', exc_info=True)
