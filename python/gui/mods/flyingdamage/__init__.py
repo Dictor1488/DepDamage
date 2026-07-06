2ba278266f83ae91093002a3bc35a53a2655a276
Dictor1488 <168811367+Dictor1488@users.noreply.github.com>
2026-07-06 15:32:56 +0300

    Use ACK based SWF polling queue

--- python/gui/mods/flyingdamage/__init__.py
+++ python/gui/mods/flyingdamage/__init__.py
@@ -19,10 +19,12 @@ _SWF_NAME = 'FlyingDamageApp.swf'
 _LINKAGE = 'FlyingDamageApp'
 _HIGH_DEPTH = 9999.0
 _MAX_QUEUE = 128
+_PULL_BATCH = 24
 _damageQueue = []
+_nextQueueID = [1]
 _pushLog = [0]
 _pullLog = [0]
-_directLog = [0]
+_ackLog = [0]
 
 
 def _fmt_float(v):
@@ -55,11 +57,36 @@ def _norm_xy(x, y):
 
 def _queue_record(record):
     try:
-        _damageQueue.append(record)
+        qid = _nextQueueID[0]
+        _nextQueueID[0] += 1
+        full = '%d|%s' % (qid, record)
+        _damageQueue.append(full)
         if len(_damageQueue) > _MAX_QUEUE:
             del _damageQueue[0:len(_damageQueue) - _MAX_QUEUE]
+        return qid
     except Exception:
         logger.error('[FlyingDamage] queue append failed', exc_info=True)
+        return 0
+
+
+def _ack_record(qid):
+    try:
+        qid = str(int(qid))
+    except Exception:
+        return False
+    removed = 0
+    try:
+        for i in xrange(len(_damageQueue) - 1, -1, -1):
+            if _damageQueue[i].split('|', 1)[0] == qid:
+                del _damageQueue[i]
+                removed += 1
+        if _ackLog[0] < 80:
+            _ackLog[0] += 1
+            logger.info('[FlyingDamage] ACK queue id=%s removed=%s left=%s', qid, removed, len(_damageQueue))
+        return removed > 0
+    except Exception:
+        logger.error('[FlyingDamage] ack failed id=%s', qid, exc_info=True)
+        return False
 
 
 class _FlyingDamageMeta(BaseDAAPIModule):
@@ -67,15 +94,17 @@ class _FlyingDamageMeta(BaseDAAPIModule):
     def py_log(self, msg):
         logger.info('[FlyingDamage] %s', msg)
 
+    def py_ackDamage(self, qid):
+        return _ack_record(qid)
+
     def py_pullDamageText(self):
         try:
             if not _damageQueue:
                 return ''
-            data = '\n'.join(_damageQueue)
-            del _damageQueue[:]
-            if _pullLog[0] < 30:
+            data = '\n'.join(_damageQueue[:_PULL_BATCH])
+            if _pullLog[0] < 60:
                 _pullLog[0] += 1
-                logger.info('[FlyingDamage] AS3 pulled damage queue: %s', data)
+                logger.info('[FlyingDamage] AS3 pulled damage queue keep=%s batch=%s data=%s', len(_damageQueue), min(len(_damageQueue), _PULL_BATCH), data)
             return data
         except Exception:
             logger.error('[FlyingDamage] py_pullDamageText failed', exc_info=True)
@@ -121,6 +150,7 @@ class FlyingDamageFlash(ExternalFlashComponent, _FlyingDamageMeta):
         try:
             self.flashObject.py_log = self.py_log
             self.flashObject.py_pullDamageText = self.py_pullDamageText
+            self.flashObject.py_ackDamage = self.py_ackDamage
             self.flashObject.py_getScreenPos = self.py_getScreenPos
             self.flashObject.py_projectWorld = self.py_projectWorld
         except Exception:
@@ -147,19 +177,6 @@ class FlyingDamageFlash(ExternalFlashComponent, _FlyingDamageMeta):
         except Exception:
             logger.error('[FlyingDamage] force depth failed', exc_info=True)
 
-    def showVehicleDamageDirect(self, vehicleID, nx, ny, damage, colorRGB, fontSize, alpha, riseMeters, lifeTime):
-        try:
-            if not self._isDAAPIInited():
-                return False
-            self.flashObject.as_showDamageVehicle(int(vehicleID), float(nx), float(ny), int(damage), int(colorRGB), int(fontSize), float(alpha), float(riseMeters), float(lifeTime))
-            if _directLog[0] < 60:
-                _directLog[0] += 1
-                logger.info('[FlyingDamage] DIRECT_SWF vehicle d=%s vid=%s norm=(%.4f,%.4f)', int(damage), int(vehicleID), float(nx), float(ny))
-            return True
-        except Exception:
-            logger.error('[FlyingDamage] DIRECT_SWF vehicle failed vid=%s d=%s', vehicleID, damage, exc_info=True)
-            return False
-
     def close(self):
         try:
             if self._isDAAPIInited():
@@ -180,7 +197,7 @@ class Controller(object):
         self._depthTicks = 0
 
     def init(self):
-        logger.info('[FlyingDamage] controller.init begin direct custom SWF vehicle anchor')
+        logger.info('[FlyingDamage] controller.init begin ACK polling custom SWF vehicle anchor')
         try:
             from .settings.config import g_config
             g_config.registerSettings()
@@ -219,14 +236,14 @@ class Controller(object):
     def _onAvatarReady(self, *a, **kw):
         self._battleMode = True
         self._depthTicks = 0
-        logger.info('[FlyingDamage] avatar ready -> delayed direct custom SWF create')
+        logger.info('[FlyingDamage] avatar ready -> delayed ACK polling custom SWF create')
         BigWorld.callback(2.0, self._createFlashDelayed)
         BigWorld.callback(3.5, self._installSuppressionSafe)
 
     def _createFlashDelayed(self):
         if not self._battleMode:
             return
-        logger.info('[FlyingDamage] delayed create direct custom SWF now')
+        logger.info('[FlyingDamage] delayed create ACK polling custom SWF now')
         self._createFlash()
         self._keepDepthOnTop()
 
@@ -297,9 +314,8 @@ class Controller(object):
         try:
             nx, ny, sw, sh = _norm_xy(x, y)
             riseNorm = max(0.01, min(0.20, float(risePixels) / sh))
-            rec = 'N|%s|%s|%d|%d|%d|%s|%s|%s' % (_fmt_float(nx), _fmt_float(ny), int(damage), int(colorRGB), int(fontSize), _fmt_float(alpha), _fmt_float(riseNorm), _fmt_float(lifeTime))
-            _queue_record(rec)
-            logger.info('[FlyingDamage] queued screen d=%s norm=(%.4f,%.4f)', int(damage), nx, ny)
+            qid = _queue_record('N|%s|%s|%d|%d|%d|%s|%s|%s' % (_fmt_float(nx), _fmt_float(ny), int(damage), int(colorRGB), int(fontSize), _fmt_float(alpha), _fmt_float(riseNorm), _fmt_float(lifeTime)))
+            logger.info('[FlyingDamage] queued screen ACK id=%s d=%s norm=(%.4f,%.4f)', qid, int(damage), nx, ny)
         except Exception:
             logger.error('[FlyingDamage] queue screen failed', exc_info=True)
 
@@ -307,20 +323,15 @@ class Controller(object):
         try:
             nx, ny, sw, sh = _norm_xy(fallbackX, fallbackY)
             if vehicleID is not None:
-                directOK = False
-                if self._flash is not None:
-                    directOK = self._flash.showVehicleDamageDirect(vehicleID, nx, ny, damage, colorRGB, fontSize, alpha, riseMeters, lifeTime)
-                if not directOK:
-                    rec = 'V|%d|%s|%s|%d|%d|%d|%s|%s|%s' % (int(vehicleID), _fmt_float(nx), _fmt_float(ny), int(damage), int(colorRGB), int(fontSize), _fmt_float(alpha), _fmt_float(riseMeters), _fmt_float(lifeTime))
-                    _queue_record(rec)
-                if _pushLog[0] < 80:
+                qid = _queue_record('V|%d|%s|%s|%d|%d|%d|%s|%s|%s' % (int(vehicleID), _fmt_float(nx), _fmt_float(ny), int(damage), int(colorRGB), int(fontSize), _fmt_float(alpha), _fmt_float(riseMeters), _fmt_float(lifeTime)))
+                if _pushLog[0] < 120:
                     _pushLog[0] += 1
-                    logger.info('[FlyingDamage] vehicle anchored d=%s vid=%s norm=(%.4f,%.4f) directOK=%s', int(damage), int(vehicleID), nx, ny, directOK)
+                    logger.info('[FlyingDamage] queued vehicle ACK id=%s d=%s vid=%s norm=(%.4f,%.4f) queue=%s', qid, int(damage), int(vehicleID), nx, ny, len(_damageQueue))
             else:
-                rec = 'W|%s|%s|%s|%s|%s|%d|%d|%d|%s|%s|%s' % (_fmt_float(wx), _fmt_float(wy), _fmt_float(wz), _fmt_float(nx), _fmt_float(ny), int(damage), int(colorRGB), int(fontSize), _fmt_float(alpha), _fmt_float(riseMeters), _fmt_float(lifeTime))
-                _queue_record(rec)
+                qid = _queue_record('W|%s|%s|%s|%s|%s|%d|%d|%d|%s|%s|%s' % (_fmt_float(wx), _fmt_float(wy), _fmt_float(wz), _fmt_float(nx), _fmt_float(ny), int(damage), int(colorRGB), int(fontSize), _fmt_float(alpha), _fmt_float(riseMeters), _fmt_float(lifeTime)))
+                logger.info('[FlyingDamage] queued world ACK id=%s d=%s norm=(%.4f,%.4f)', qid, int(damage), nx, ny)
         except Exception:
-            logger.error('[FlyingDamage] direct/queue vehicle failed', exc_info=True)
+            logger.error('[FlyingDamage] queue vehicle failed', exc_info=True)
 
     def showDamage(self, x, y, damage, colorRGB, fontSize, alpha):
         self.showDamageAt(x, y, damage, colorRGB, fontSize, alpha)
