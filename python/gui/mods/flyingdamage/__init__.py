f64edda014f64ed58e4c35dc9255038e5d978a0d
Dictor1488 <168811367+Dictor1488@users.noreply.github.com>
2026-07-07 13:32:42 +0300

    Fix AS3 import path

--- python/gui/mods/flyingdamage/__init__.py
+++ python/gui/mods/flyingdamage/__init__.py
@@ -1,4 +1,2 @@
 # -*- coding: utf-8 -*-
-# AS3 renderer entry. Heavy Gameface single-window code was removed.
-
-from fd_as3_controller import g_controller
+from gui.mods.fd_as3_controller import g_controller
