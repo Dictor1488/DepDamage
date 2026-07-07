7958cb95dc8c6ed8b3b9fd50f6aab0798fbb4576
Dictor1488 <168811367+Dictor1488@users.noreply.github.com>
2026-07-07 13:21:07 +0300

    Update entry logs for AS3 renderer

--- python/gui/mods/mod_flyingdamage.py
+++ python/gui/mods/mod_flyingdamage.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
-# mod_flyingdamage.py  --  Python 2.7 entry point (Gameface renderer)
+# mod_flyingdamage.py -- Python 2.7 entry point (AS3 marker-layer renderer)
 
 import logging
 
 logger = logging.getLogger(__name__)
-logger.info('[FlyingDamageGF] === module imported (Gameface renderer) ===')
+logger.info('[FD_AS3] === module imported ===')
 
 
 def init():
-    logger.info('[FlyingDamageGF] init() called')
+    logger.info('[FD_AS3] init() called')
     try:
         from flyingdamage import g_controller
         g_controller.init()
-        logger.info('[FlyingDamageGF] init() OK')
+        logger.info('[FD_AS3] init() OK')
     except Exception:
-        logger.error('[FlyingDamageGF] init() CRASHED', exc_info=True)
+        logger.error('[FD_AS3] init() CRASHED', exc_info=True)
 
 
 def fini():
-    logger.info('[FlyingDamageGF] fini() called')
+    logger.info('[FD_AS3] fini() called')
     try:
         from flyingdamage import g_controller
         g_controller.fini()
     except Exception:
-        logger.error('[FlyingDamageGF] fini() CRASHED', exc_info=True)
+        logger.error('[FD_AS3] fini() CRASHED', exc_info=True)
