39a75e9b6a5c2eaed4265aa87d024abf1371970e
Dictor1488 <168811367+Dictor1488@users.noreply.github.com>
2026-07-08 19:06:06 +0300

    Make Python entrypoint resource-patch only

--- python/gui/mods/mod_flyingdamage.py
+++ python/gui/mods/mod_flyingdamage.py
@@ -1,26 +1,17 @@
 # -*- coding: utf-8 -*-
-# mod_flyingdamage.py -- Python 2.7 entry point (AS3 marker-layer renderer)
+# mod_flyingdamage.py -- Python 2.7 entry point
+# The mod now ships the provided battleVehicleMarkersApp.swf directly.
+# No Python damage renderer, no overlay SWF, no damage suppression hook.
 
 import logging
 
 logger = logging.getLogger(__name__)
-logger.info('[FD_AS3] === module imported ===')
+logger.info('[FlyingDamage] module imported: battleVehicleMarkersApp.swf resource patch only')
 
 
 def init():
-    logger.info('[FD_AS3] init() called')
-    try:
-        from flyingdamage import g_controller
-        g_controller.init()
-        logger.info('[FD_AS3] init() OK')
-    except Exception:
-        logger.error('[FD_AS3] init() CRASHED', exc_info=True)
+    logger.info('[FlyingDamage] init: no Python damage hooks; using provided battleVehicleMarkersApp.swf')
 
 
 def fini():
-    logger.info('[FD_AS3] fini() called')
-    try:
-        from flyingdamage import g_controller
-        g_controller.fini()
-    except Exception:
-        logger.error('[FD_AS3] fini() CRASHED', exc_info=True)
+    logger.info('[FlyingDamage] fini')
