672ed9da824c1b23ae80155dd5d1b5b1583b5c18
Dictor1488 <168811367+Dictor1488@users.noreply.github.com>
2026-07-17 20:10:08 +0300

    Enable partial tank-follow motion

--- python/gui/mods/mod_flyingdamage.py
+++ python/gui/mods/mod_flyingdamage.py
@@ -6,6 +6,7 @@ Python 2.7 client-side WoT mod entry.
 
 import logging
 
+from flyingdamage import follow
 from flyingdamage import hooks
 from flyingdamage import settings
 
@@ -15,10 +16,12 @@ LOG = logging.getLogger('DepDamage')
 def init():
     LOG.info('[DepDamage] init')
     hooks.init()
+    follow.init()
     settings.init()
 
 
 def fini():
     LOG.info('[DepDamage] fini')
     settings.fini()
+    follow.fini()
     hooks.fini()
